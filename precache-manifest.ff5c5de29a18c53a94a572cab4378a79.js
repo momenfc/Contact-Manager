76e469ce707d1b6edb66084cda53c397c501c161
Your Name <you@example.com>
2020-10-30 07:13:21 +0200

    Updates

---
+++ precache-manifest.ff5c5de29a18c53a94a572cab4378a79.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e05afa9592769a6172f388f59f9359e3",
+    "url": "/momenfc/Contact-Manager.git/index.html"
+  },
+  {
+    "revision": "127b6c3d098b595a3f7e",
+    "url": "/momenfc/Contact-Manager.git/static/css/2.40243d22.chunk.css"
+  },
+  {
+    "revision": "30fd5c6b1fad59d1ae8c",
+    "url": "/momenfc/Contact-Manager.git/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "127b6c3d098b595a3f7e",
+    "url": "/momenfc/Contact-Manager.git/static/js/2.bb633d8a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/momenfc/Contact-Manager.git/static/js/2.bb633d8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30fd5c6b1fad59d1ae8c",
+    "url": "/momenfc/Contact-Manager.git/static/js/main.9ccc046c.chunk.js"
+  },
+  {
+    "revision": "eedd352fcd63c8b385d7",
+    "url": "/momenfc/Contact-Manager.git/static/js/runtime-main.1ca9a4ef.js"
+  },
+  {
+    "revision": "2f12242375edd68e9013ecfb59c672e9",
+    "url": "/momenfc/Contact-Manager.git/static/media/fa-brands-400.2f122423.svg"
+  },
+  {
+    "revision": "8300bd7f30e0a313c1d772b49d96cb8e",
+    "url": "/momenfc/Contact-Manager.git/static/media/fa-brands-400.8300bd7f.ttf"
+  },
+  {
+    "revision": "ad527cc5ec23d6da66e8a1d6772ea6d3",
+    "url": "/momenfc/Contact-Manager.git/static/media/fa-brands-400.ad527cc5.woff"
+  },
+  {
+    "revision": "e2ca6541bff3a3e9f4799ee327b28c58",
+    "url": "/momenfc/Contact-Manager.git/static/media/fa-brands-400.e2ca6541.eot"
+  },
+  {
+    "revision": "f075c50f89795e4cdb4d45b51f1a6800",
+    "url": "/momenfc/Contact-Manager.git/static/media/fa-brands-400.f075c50f.woff2"
+  },
+  {
+    "revision": "3602b7e8b2cb1462b0bef9738757ef8a",
+    "url": "/momenfc/Contact-Manager.git/static/media/fa-regular-400.3602b7e8.svg"
+  },
+  {
+    "revision": "3c6879c4f342203d099bdd66dce6d396",
+    "url": "/momenfc/Contact-Manager.git/static/media/fa-regular-400.3c6879c4.woff"
+  },
+  {
+    "revision": "49f00693b0e5d45097832ef5ea1bc541",
+    "url": "/momenfc/Contact-Manager.git/static/media/fa-regular-400.49f00693.ttf"
+  },
+  {
+    "revision": "4a74738e7728e93c4394b8604081da62",
+    "url": "/momenfc/Contact-Manager.git/static/media/fa-regular-400.4a74738e.woff2"
+  },
+  {
+    "revision": "b01516c1808be557667befec76cd6318",
+    "url": "/momenfc/Contact-Manager.git/static/media/fa-regular-400.b01516c1.eot"
+  },
+  {
+    "revision": "205f07b3883c484f27f40d21a92950d4",
+    "url": "/momenfc/Contact-Manager.git/static/media/fa-solid-900.205f07b3.ttf"
+  },
+  {
+    "revision": "4451e1d86df7491dd874f2c41eee1053",
+    "url": "/momenfc/Contact-Manager.git/static/media/fa-solid-900.4451e1d8.woff"
+  },
+  {
+    "revision": "664de3932dd6291b4b8a8c0ddbcb4c61",
+    "url": "/momenfc/Contact-Manager.git/static/media/fa-solid-900.664de393.svg"
+  },
+  {
+    "revision": "8ac3167427b1d5d2967646bd8f7a0587",
+    "url": "/momenfc/Contact-Manager.git/static/media/fa-solid-900.8ac31674.eot"
+  },
+  {
+    "revision": "8e1ed89b6ccb8ce41faf5cb672677105",
+    "url": "/momenfc/Contact-Manager.git/static/media/fa-solid-900.8e1ed89b.woff2"
+  }
+]);
